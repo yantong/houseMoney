e7a85f649a78e16263bc638d3ee50e0fe5678147
yantong <1209144177@qq.com>
2020-03-12 16:04:26 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,103 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <Input v-model="money" placeholder="本金" style="width: 100px;margin: 10px" />
+    <Input v-model="monRate" placeholder="年利率" style="width: 100px;margin: 10px" />
+    <Input v-model="mon" placeholder="还款期数" style="width: 100px;margin: 10px" />
+    <Button @click="showGraphData" type="primary" style="width: 80px;margin: 10px">查看</Button>
+
+    <div ref="showTable" style="height:800px;width: 100%"></div>
   </div>
 </template>
 
 <script>
+import echarts from 'echarts'
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      money: 490000,
+      monRate: 0.1 || 0.049 * 1.15,
+      mon: 360
+    }
+  },
+  mounted() {
+    this.showGraphData()
+  },
+  methods: {
+    getPrice(money,monRate,mon) {
+        return (money*monRate* Math.pow(1+monRate, mon)) / (Math.pow(1+monRate, mon) - 1.0 )
+    },
+    showGraphData () {
+      const _this = this
+
+
+
+      if(isNaN(+this.money) || isNaN(+this.monRate) || isNaN(+this.mon) || (!this.money || !this.monRate || !this.mon))
+        this.$Message.error('只能输入数字')
+
+      let rate = []
+
+      for (let index = 0.001; index < this.monRate; ) {
+        rate.push(index)
+
+        index += 0.001
+      }
+
+      let option = {
+        title: {
+          text: ''
+        },
+        xAxis: [
+          {
+            type: 'category',
+            boundaryGap: false,
+            data: rate.map(item => {
+              return (item - 0.049 * 1.15).toFixed(5) * 1.0
+            })
+          }
+        ],
+        tooltip: {
+            trigger: 'axis',
+            formatter(params) {
+              let x = params[0].axisValue
+              let y = params[0].value
+
+              return `利率差为： ${(+x).toFixed(3)} 还款差为：${y}`
+            },
+        },
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: '',
+            type: 'line',
+            label: {
+              normal: {
+                // show: true,
+                // position: 'top'
+              }
+            },
+            data: rate.map((item) => {
+              return ((_this.getPrice(+_this.money,+item / 12,+_this.mon) - _this.getPrice(490000,0.049 * 1.15 / 12,360)) * 360).toFixed(2)
+            })
+          }
+        ]
+      }        
+
+      if (!_this.dom) 
+        _this.dom = echarts.init(_this.$refs.showTable) 
+
+      _this.dom.setOption(option)
+    },
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
